3cd88cd1299a4c2c72343c43e6e249a66b2c180f
Rowan-Laffeber <9026051@student.zadkine.nl>
2025-05-31 20:10:06 +0200

    v0.0.1
    data on the car detail page now comes  out of the database. also made de url pretty using slugs and htaccess rewrites for a better customer experience

--- pages/car-detail.php
+++ pages/car-detail.php
@@ -1,4 +1,4 @@
-<?php require "includes/header.php" ?>
+
 
 <?php
 //TODO: Implementeer dat de pagina de juiste auto laat zien op basis van de query paramater 'name'
@@ -19,34 +19,75 @@
             <div class="advertorial">
                 <h2>Sport auto met het beste design en snelheid</h2>
                 <p>Veiligheid en comfort terwijl je rijd in een futiristische en elante auto </p>
-                <img src="assets/images/car-rent-header-image-1.png" alt="">
-                <img src="assets/images/header-circle-background.svg" alt="" class="background-header-element">
+                <img src="/assets/images/car-rent-header-image-1.png" alt="">
+                <img src="/assets/images/header-circle-background.svg" alt="" class="background-header-element">
             </div>
         </div>
+        <?php
+        try {
+            if (isset($_GET['slug'], $_GET['id']) && is_numeric($_GET['id'])) {
+                $carId = (int)$_GET['id'];
+                $slugFromUrl = $_GET['slug'];
+
+                $get_car = $conn->prepare("SELECT * FROM car WHERE id = :id LIMIT 1");
+                $get_car->bindParam(':id', $carId, PDO::PARAM_INT);
+                $get_car->execute();
+                $car = $get_car->fetch(PDO::FETCH_ASSOC);
+
+                if ($car) {
+                    $brand = htmlspecialchars($car['brand']);
+                    $type = htmlspecialchars($car['type']);
+                    $class = htmlspecialchars($car['class']);
+                    $seats = htmlspecialchars($car['seats']);
+                    $transmission = htmlspecialchars($car['transmission']);
+                    $tank_capacity = htmlspecialchars($car['tank_capacity']);
+                    $price_day = htmlspecialchars($car['price_day']);
+
+                    $expectedSlug = strtolower($brand . '-' . $type);
+                    $expectedSlug = preg_replace('/[^a-z0-9\-]/', '', str_replace(' ', '-', $expectedSlug));
+
+                    if ($slugFromUrl !== $expectedSlug) {
+                        header("Location: /car-detail/{$expectedSlug}-{$carId}");
+                        exit;
+                    }
+                } else {
+                    echo "<p>Auto niet gevonden.</p>";
+                    exit;
+                }
+            } else {
+                echo "<p>Ongeldige ID-parameter.</p>";
+                exit;
+            }
+        } catch (PDOException $e) {
+            echo "<p>Fout bij het ophalen van de auto: " . htmlspecialchars($e->getMessage()) . "</p>";
+            exit;
+        }
+        ?>
+
         <div class="row white-background">
-            <h2>Nissan GT-R</h2>
+            <h2><?= $brand ?> <?= $type ?></h2>
             <div class="rating">
                 <span class="stars stars-4"></span>
                 <span>440+ reviewers</span>
             </div>
-            <p>NISMO is het toonbeeld geworden van Nissan's uitzonderlijke prestaties, geïnspireerd door het meest meedogenloze testterrein: het circuit.</p>
+            <p><?= $brand ?> <?= $type ?> is het toonbeeld geworden van <?= $brand ?>'s uitzonderlijke prestaties, geïnspireerd door het meest meedogenloze testterrein: het circuit.</p>
+            
             <div class="car-type">
                 <div class="grid">
-                    <div class="row"><span class="accent-color">Type Car</span><span>Sport</span></div>
-                    <div class="row"><span class="accent-color">Seats</span><span>2</span></div>
+                    <div class="row"><span class="accent-color">Type Car</span><span><?= $class ?></span></div>
+                    <div class="row"><span class="accent-color">Seats</span><span><?= $seats ?></span></div>
                 </div>
                 <div class="grid">
-                    <div class="row"><span class="accent-color">Transmission</span><span>Manual</span></div>
-                    <div class="row"><span class="accent-color">Fuel / Capacity</span><span>Gasoline / 70L</span></div>
+                    <div class="row"><span class="accent-color">Transmission</span><span><?= $transmission ?></span></div>
+                    <div class="row"><span class="accent-color">Fuel / Capacity</span><span><?= $tank_capacity ?></span></div>
                 </div>
                 <div class="call-to-action">
-                    <div class="row"><span class="font-weight-bold">€80,00</span> / dag</div>
-                    <div class="row"><a href="" class="button-primary">Huur nu</a></div>
+                    <div class="row"><span class="font-weight-bold">€<?= $price_day ?></span> / dag</div>
+                    <div class="row"><a href="/rent?id=<?= $carId ?>" class="button-primary">Huur nu</a></div>
                 </div>
-
             </div>
         </div>
     </div>
 </main>
 
-<?php require "includes/footer.php" ?>
+
